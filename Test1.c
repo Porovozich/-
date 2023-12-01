0b4a0f3f6b9cb32abdab94a4b1c6e6e433f8a042
Porovozich <148583540+Porovozich@users.noreply.github.com>
2023-12-01 21:19:27 +0300

    Add files via upload

--- Test1.c
+++ Test1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-	printf("Hello World!\n");
+	float g;
+	float a;
+	float x;
+	float q;
+	float t;
+	float e;
+	float z;
+	for(a=2;a<=4;a= a + 1.3) {
+		for(x=1;x<=3;x = x + 1.3) {
+			q = sqrt(tan((a - x)/x));
+			t = log((a * a)/x);
+			e = atan((x * cbrt(a))/(a + 2,5));
+			g = fmin(q,t);
+			z = fmin(g,e);
+			printf("\n x = %f a = %f z = %f \n",x,a,z);
+		}
+		}
 	return 0;
 }
